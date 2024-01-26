6262b487a65778c9a5780436ab9e569f18cfbb0d
lyh990517 <lyh990517@naver.com>
2024-01-26 13:52:03 +0900

    Remove Imports

--- app/src/main/java/com/example/compose_calendar/MainActivity.kt
+++ app/src/main/java/com/example/compose_calendar/MainActivity.kt
@@ -8,10 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calendar.Calendar
 import com.example.compose_calendar.ui.theme.ComposecalendarTheme
